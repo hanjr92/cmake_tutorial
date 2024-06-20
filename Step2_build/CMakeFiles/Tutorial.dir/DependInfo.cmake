965e3dacda919421b30efa37b73351f52bbc1cec
hanjr92 <jiaronghan@outlook.com>
2024-06-20 15:29:02 +0800

    update

---
+++ Step2_build/CMakeFiles/Tutorial.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/data1/naohai_pretrain_team4_clean/hanjr/test/cmake/Step2/tutorial.cxx" "/data1/naohai_pretrain_team4_clean/hanjr/test/cmake/Step2_build/CMakeFiles/Tutorial.dir/tutorial.cxx.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/data1/naohai_pretrain_team4_clean/hanjr/test/cmake/Step2/MathFunctions"
+  "."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/data1/naohai_pretrain_team4_clean/hanjr/test/cmake/Step2_build/MathFunctions/CMakeFiles/MathFunctions.dir/DependInfo.cmake"
+  "/data1/naohai_pretrain_team4_clean/hanjr/test/cmake/Step2_build/MathFunctions/CMakeFiles/SqrtLibrary.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Step2_build/MathFunctions/CMakeFiles/SqrtLibrary.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/data1/naohai_pretrain_team4_clean/hanjr/test/cmake/Step2/MathFunctions/mysqrt.cxx" "/data1/naohai_pretrain_team4_clean/hanjr/test/cmake/Step2_build/MathFunctions/CMakeFiles/SqrtLibrary.dir/mysqrt.cxx.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
